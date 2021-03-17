072224ea676c0f7e46afd83c47657a0b517554e6
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-03-17 18:41:06 -0400

    commit tree c0d182803618f2d71746031be2a2415f072aa207

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+// function superbowlWin(element, index, array) {
+//     if (element["result"] === "W") {
+//         return element["year"]
+//     }
+// }
+
+// function superbowlWin(recordsArray) {
+//     recordsArray.find(function (record) {
+//         if (record.result === "W") {
+//             return record.year
+//         }
+//     })
+// }
+
+function superbowlWin(recordsArray) {
+    let returnItem
+    recordsArray.find(function (record) {
+        if (record.result === "W") {
+            returnItem = record.year
+            //console.log(returnItem)
+        }
+    })
+    //console.log(returnItem)
+    return returnItem
+}
